cf6ab99f751bbb5bcea11c9d2709e59b9c4c5428
AsakuraMizu <asakuramizu111@gmail.com>
2024-03-01 19:10:33 +0800

    update

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+mkdir src/images/{qifeng,yindeng,nihua,banners}
+for f in opticalnotes-images/启封/*; do cp $f src/images/qifeng/${f:29}; done
+for f in opticalnotes-images/引灯/*; do cp $f src/images/yindeng/${f:29}; done
+for f in opticalnotes-images/拟画/*; do cp $f src/images/nihua/${f:29}; done
+for f in opticalnotes-images/头图/*; do cp $f src/images/banners/${f:23}; done
 pnpm svelte-kit sync
 pnpm tsx src/update.cts
 pnpm build:site
